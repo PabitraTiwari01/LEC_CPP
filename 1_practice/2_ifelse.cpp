0d8eccbf621e6c82661a295af9d814245b3cd09d
Pabitra <piscopabitra0@gmail.com>
2023-05-24 10:02:14 +0545

    first chapter

---
+++ 1_practice/2_ifelse.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int dig1,dig2,dig3;
+    cout<<"Enter threee integers:";
+    cin>>dig1>>dig2>>dig3;
+    if(dig1>dig2)
+        if(dig1>dig2)
+        cout<<dig1<<"Is the largest:";
+            else
+            cout<<dig3<<"Is the largest";
+        else    
+            if(dig2>dig3)
+            cout<<dig2<<"Is the largest";
+            else
+            cout<<dig3<<"Is the largest";
+            system("pause");
+            return 0;    
+}
